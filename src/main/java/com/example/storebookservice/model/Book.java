658eb96b3189625e0df592d0d4cb0c67da7c0567
saifat <ivan@vizgalin.ru>
2023-07-29 11:06:44 +0300

    first commit

---
+++ src/main/java/com/example/storebookservice/model/Book.java
@@ -0,0 +1,25 @@
+package com.example.storebookservice.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Book {
+
+    private int id;
+    private String name;
+    private String description;
+    private String status;
+    private int price;
+
+    @Builder
+    public Book(int id, String name, String description, String status, int price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.status = status;
+        this.price = price;
+    }
+}
